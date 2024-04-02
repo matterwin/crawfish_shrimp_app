258dd9e1abd28cfa69a338ec08f2402b18ab6ee0
matterwin <matthewderwin23@gmail.com>
2024-04-02 18:13:46 -0500

    spring boot init setup

---
+++ App/server/crawfish/src/main/java/com/example/crawfish/CrawfishApplication.java
@@ -0,0 +1,13 @@
+package com.example.crawfish;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrawfishApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrawfishApplication.class, args);
+	}
+
+}
---
+++ App/server/crawfish/src/test/java/com/example/crawfish/CrawfishApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.crawfish;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrawfishApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
